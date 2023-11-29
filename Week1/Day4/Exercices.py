63a429baeeb7eb35ad8a370262038276eb50f2ac
Your Name <voronina1504@gmai.com>
2023-11-29 18:21:30 +0300

    relevant message

---
+++ Week1/Day4/Exercices.py
@@ -0,0 +1,69 @@
+# Exercise 1 : What Are You Learning ?
+# Instructions
+# Write a function called display_message() that prints one sentence 
+# telling everyone what you are learning in this course.
+# Call the function, and make sure the message displays correctly.
+
+#def display_message():
+#    print("I'm learning in developers.institute.")
+
+#display_message()
+
+# Exercise 2: What’s Your Favorite Book ?
+# Instructions
+# Write a function called favorite_book() that accepts one parameter called title.
+# The function should print a message, such as "One of my favorite books is <title>".
+# For example: “One of my favorite books is Alice in Wonderland”
+# Call the function, make sure to include a book title as an argument when 
+# calling the function.
+
+#def favorite_book(title):
+#    print("One of my favorite books is " + title)
+
+# favorite_book("Alice in Wonderland")
+
+# Exercise 3 : Some Geography
+# Instructions
+# Write a function called describe_city() that accepts the name of a city 
+# and its country as parameters.
+# The function should print a simple sentence, such as "<city> is in <country>".
+# For example “Reykjavik is in Iceland”
+# Give the country parameter a default value.
+# Call your function.
+
+#def describe_city(city, country="Some country"):
+#    print(city + " is in " + country)
+
+# describe_city("Reykjavik", "Iceland")
+# describe_city("Rio")
+
+# Exercise 4 : Random
+# Instructions
+# Create a function that accepts a number between 1 and 100 and generates 
+# another number randomly between 1 and 100. Use the random module.
+# Compare the two numbers, if it’s the same number, display a success message, 
+# otherwise show a fail message and display both numbers.
+
+# import random
+
+#def compare_numbers(user_number):
+#    random_number = random.randint(1, 100)
+#    print("Generated random number:", random_number)
+
+#    if user_number == random_number:
+#        print("Success! The numbers match.")
+#    else:
+#        print("Fail! The numbers don't match.")
+    
+#    print(f"User's number: {user_number}, random number: {random_number}")
+
+#user_input = int(input("Enter a number between 1 and 100: "))
+#compare_numbers(user_input)
+
+
+
+
+
+
+
+
---
+++ training.py
@@ -0,0 +1,27 @@
+# l1 = [1, 2, 3, 4, 5, 6]
+# l1.append(3)
+# l1.append([7, 8, 9])
+# l1.extend([6, 7, 8])
+# print(l1)
+
+# l1=[1, 2, 3, 4, 5, 6]
+# l1.insert(2, 6)
+# print(l1)
+
+l2 = [1, 2, 3, 4, 5, "a", "a", "b", 1, 2, 4]
+s = l2.count(4)
+# print(s)
+
+a = ["b", "g", "a", "d", "f", "c", "h", "e"]
+x = sorted(a)
+# print("a after sorted function")
+# print(a)
+# print(x)
+b = [1, 2, 5, 8, 3]
+b.sort()
+# print(b)
+
+myList = [10, 25, 17, 9, 30, -5]
+# Double the value of each element
+myList2 = map(lambda n : n*2, myList)
+print(myList2)
