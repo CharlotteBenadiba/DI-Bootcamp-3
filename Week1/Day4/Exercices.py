a145d2e504c7f56335a1478b157edfb442560d7e
Your Name <voronina1504@gmai.com>
2023-11-29 20:46:54 +0300

    relevant message

--- Week1/Day4/Exercices.py
+++ Week1/Day4/Exercices.py
@@ -60,6 +60,144 @@
 #user_input = int(input("Enter a number between 1 and 100: "))
 #compare_numbers(user_input)
 
+# Exercise 5 : Let’s Create Some Personalized Shirts !
+# Instructions
+# Write a function called make_shirt() that accepts a size and the text 
+# of a message that should be printed on the shirt.
+# The function should print a sentence summarizing the size of the shirt 
+# and the message printed on it, such as "The size of the shirt is <size> 
+# and the text is <text>"
+# Call the function make_shirt().
+# Modify the make_shirt() function so that shirts are large by default 
+# with a message that reads “I love Python” by default.
+# Make a large shirt with the default message
+# Make medium shirt with the default message
+# Make a shirt of any size with a different message.
+# Bonus: Call the function make_shirt() using keyword arguments.
+
+#def make_shirt(size="large", text="I love Python"):
+#    print(f"The size of the shirt is {size} and the text is '{text}'")
+
+#make_shirt()
+#make_shirt(size="medium")
+#make_shirt(size="small", text="Python is cool!")
+
+# Bonus:
+#make_shirt(text="I love developers.institute!", size="extra-large")
+
+#  Exercise 6 : Magicians …
+# Instructions
+# Using this list of magician’s names
+# magician_names = ['Harry Houdini', 'David Blaine', 'Criss Angel']
+# Create a function called show_magicians(), which prints the name of each 
+# magician in the list.
+# Write a function called make_great() that modifies the original list 
+# of magicians by adding the phrase "the Great" to each magician’s name.
+# Call the function make_great().
+# Call the function show_magicians() to see that the list has actually been modified.
+
+#def show_magicians(magicians):
+#    for magician in magicians:
+#        print(magician)
+
+#def make_great(magicians):
+
+#    for i in range(len(magicians)):
+#        magicians[i] += " the Great"
+
+# magician_names = ['Harry Houdini', 'David Blaine', 'Criss Angel']
+# show_magicians(magician_names)
+
+# make_great(magician_names)
+# show_magicians(magician_names)
+
+# Exercise 7 : Temperature Advice
+# Instructions
+# Create a function called get_random_temp().
+# This function should return an integer between -10 and 40 degrees (Celsius), 
+# selected at random.
+# Test your function to make sure it generates expected results.
+# Create a function called main().
+# Inside this function, call get_random_temp() to get a temperature, and store 
+# its value in a variable.
+# Inform the user of the temperature in a friendly message, eg. “The temperature 
+# right now is 32 degrees Celsius.”
+# Let’s add more functionality to the main() function. Write some friendly 
+# advice relating to the temperature:
+# below zero (eg. “Brrr, that’s freezing! Wear some extra layers today”)
+# between zero and 16 (eg. “Quite chilly! Don’t forget your coat”)
+# between 16 and 23
+# between 24 and 32
+# between 32 and 40
+# Change the get_random_temp() function:
+# Add a parameter to the function, named ‘season’.
+# Inside the function, instead of simply generating a random number between 
+# -10 and 40, set lower and upper limits based on the season, eg. if season 
+# is ‘winter’, temperatures should only fall between -10 and 16.
+# Now that we’ve changed get_random_temp(), let’s change the main() function:
+# Before calling get_random_temp(), we will need to decide on a season, so that 
+# we can call the function correctly. Ask the user to type in a season - 
+# ‘summer’, ‘autumn’ (you can use ‘fall’ if you prefer), ‘winter’, or ‘spring’.
+# Use the season as an argument when calling get_random_temp().
+# Bonus: Give the temperature as a floating-point number instead of an integer.
+# Bonus: Instead of asking for the season, ask the user for the number of the 
+# month (1 = January, 12 = December). Determine the season according to the month.
+
+# import random
+
+#def get_random_temp(input_value):
+#    valid_seasons = ['summer', 'autumn', 'fall', 'winter', 'spring']
+
+#    if input_value.isdigit() and 1 <= int(input_value) <= 12:
+#        month = int(input_value)
+#        if 6 <= month <= 8: 
+#            return round(random.uniform(16, 40), 1)
+#        elif 9 <= month <= 11 or 1 <= month <= 2:  
+#            return round(random.uniform(0, 23), 1)
+#        elif 12 <= month <= 2 or 3 <= month <= 5:  
+#            return round(random.uniform(-10, 16), 1)
+#        elif 6 <= month <= 8 or 9 <= month <= 11:  
+#            return round(random.uniform(0, 23), 1)
+#    elif input_value.lower() in valid_seasons:
+#        season = input_value.lower()
+#        if season == 'summer':
+#            return round(random.uniform(16, 40), 1)
+#        elif season == 'autumn' or season == 'fall':
+#            return round(random.uniform(0, 23), 1)
+#        elif season == 'winter':
+#            return round(random.uniform(-10, 16), 1)
+#        elif season == 'spring':
+#            return round(random.uniform(0, 23), 1)
+#    else:
+#        return None
+
+#def main():
+    
+    #input_value = input("Enter the season ('summer', 'autumn', 'winter', 'spring') or the number of the month (1-12): ")
+    #temperature = get_random_temp(input_value)
+
+    #if temperature is not None:
+    #    print(f"The temperature right now is {temperature} degrees Celsius.")
+
+    #    if temperature < 0:
+    #        print("Brrr, that's freezing! Wear some extra layers today.")
+    #    elif 0 <= temperature < 16:
+    #        print("Quite chilly! Don't forget your coat.")
+    #    elif 16 <= temperature < 23:
+    #        print("It's a pleasant day. Enjoy the weather!")
+    #    elif 24 <= temperature < 32:
+    #        print("Warm weather! Stay hydrated.")
+    #    elif 32 <= temperature <= 40:
+    #        print("Hot day! Consider staying indoors or using sunscreen.")
+    #    else:
+     #       print("There are no recommendations for this temperature.")
+    #else:
+    #    print("Incorrect input. Please write a valid season or month.")
+
+# main()
+
+ 
+
 
 
 
