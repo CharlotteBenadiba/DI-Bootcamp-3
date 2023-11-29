3dacb3b31774c48c9eefbe769016092e678290c4
Your Name <voronina1504@gmai.com>
2023-11-29 21:12:18 +0300

    relevant message

--- Week1/Day4/Exercices.py
+++ Week1/Day4/Exercices.py
@@ -196,7 +196,70 @@
 
 # main()
 
- 
+ # Exercise 8 : Star Wars Quiz
+# Instructions
+# This project allows users to take a quiz to test their Star Wars knowledge.
+# The number of correct/incorrect answers are tracked and the user receives 
+# different messages depending on how well they did on the quiz.
+# Here is an array of dictionaries, containing those questions and answers
+
+# Create a function that asks the questions to the user, and check his answers. Track the number of correct, incorrect answers. Create a list of wrong_answers
+# Create a function that informs the user of his number of correct/incorrect answers.
+# Bonus : display to the user the questions he answered wrong, his answer, and the correct answer.
+# If he had more then 3 wrong answers, ask him to play again.
+
+# import random
+
+# data = [
+#    {"question": "What is Baby Yoda's real name?", "answer": "Grogu"},
+#    {"question": "Where did Obi-Wan take Luke after his birth?", "answer": "Tatooine"},
+#    {"question": "What year did the first Star Wars movie come out?", "answer": "1977"},
+#    {"question": "Who built C-3PO?", "answer": "Anakin Skywalker"},
+#    {"question": "Anakin Skywalker grew up to be who?", "answer": "Darth Vader"},
+#    {"question": "What species is Chewbacca?", "answer": "Wookiee"}
+#]
+
+#def quiz():
+#    correct_answers = 0
+#    incorrect_answers = 0
+#    wrong_answers = []
+
+#    random.shuffle(data) 
+
+#    for question_data in data:
+#        question = question_data["question"]
+#        correct_answer = question_data["answer"]
+
+#        user_answer = input(question + " ")
+#        user_answer = user_answer.strip()
+
+#        if user_answer.lower() == correct_answer.lower():
+#            print("Correct!\n")
+#            correct_answers += 1
+#        else:
+#            print(f"Incorrect. The correct answer is {correct_answer}.\n")
+#            incorrect_answers += 1
+#            wrong_answers.append({"question": question, "user_answer": user_answer, "correct_answer": correct_answer})
+
+ #   show_results(correct_answers, incorrect_answers, wrong_answers)
+
+#def show_results(correct, incorrect, wrong_answers):
+#    print("=== Quiz Results ===")
+#    print(f"Correct Answers: {correct}")
+#    print(f"Incorrect Answers: {incorrect}")
+
+#    if incorrect > 0:
+#        print("\n=== Questions You Answered Incorrectly ===")
+#        for wrong_answer in wrong_answers:
+#            print(f"Question: {wrong_answer['question']}")
+#            print(f"Your Answer: {wrong_answer['user_answer']}")
+#            print(f"Correct Answer: {wrong_answer['correct_answer']}\n")
+
+#    if incorrect > 3:
+#        print("You had more than 3 wrong answers. Consider playing again.")
+
+# quiz()
+
 
 
 
